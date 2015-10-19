15c3785d637f98e927204182b1936f60d8b25e85
Lucas BAUJARD <lubaujar@e1r9p18.42.fr>
2015-10-19 14:43:31 +0200

    Mon 14:45

---
+++ angular_js/todolist_grafikart/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/todolist', function(req, res) {
+	res.render('index.ejs');
+})
+
+.listen(2121, function() {
+	console.log('Server running on: 127.0.0.1:2121');
+	console.log('Run: http://127.0.0.1/todolist');
+})
