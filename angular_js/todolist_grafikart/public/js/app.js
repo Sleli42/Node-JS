8a249a945fb4f27025a57b7cdeaac9fcb1bb2b13
Lucas BAUJARD <lubaujar@e1r13p10.42.fr>
2015-10-20 16:41:06 +0200

    Tue 16:41

--- angular_js/todolist_grafikart/public/js/app.js
+++ angular_js/todolist_grafikart/public/js/app.js
@@ -1,12 +1,39 @@
-function TodoCtrl($scope) {
-	$scope.todos = [
-		{
-			name : "Tache incomplete",
+var app = angular.module('Todo', []);
+
+function TodoCtrl($scope, filterFilter, $http) {
+	$scope.todos = [];
+	$scope.placeholder = "Chargement...";
+
+	$http.get("todo.php").success(function(data) {
+		$scope.todos = data;
+		$scope.placeholder = "Nouvelle Tâche";
+	})
+
+	$scope.$watch('todos', function() {
+		$scope.remaining = filterFilter($scope.todos,
+			{completed:false}).length;
+		$scope.allCheck = !$scope.remaining;
+	}, true)
+
+	$scope.removeTodo = function(index) {
+		$scope.todos.splice(index, 1);
+	}
+
+	$scope.addTodo = function() {
+		$scope.todos.push({
+			name : $scope.newTodo,
 			completed : false
-		},
-		{
-			name : "Tache complete",
-			completed : true
-		}
-	]
+		});
+		$scope.newTodo = '';
+	}
+
+	$scope.editTodo = function(todo) {
+		todo.editing = false;
+	}
+
+	$scope.checkAllTodo = function(allCheck) {
+		$scope.todos.forEach(function(todo) {
+			todo.completed = allCheck;
+		})
+	}
 }
---
+++ angular_js/todolist_grafikart/public/php/todo.php
@@ -0,0 +1,6 @@
+[
+	{"name": "1er todo a faire", "completed": false},
+	{"name": "2nd todo a faire", "completed": false},
+	{"name": "3eme todo a faire", "completed": false},
+	{"name": "dernier todo a faire", "completed": false}
+]
